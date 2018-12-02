4e82bfe78d34b317f6fcfefb6b539bf7ecbec0b9
Batschko <lukas.h2@hotmail.de>
2018-12-02 19:18:00 +0100

    Player update

---
+++ src/Board/Board.java
@@ -0,0 +1,16 @@
+package Board;
+
+
+public class Board{
+	
+	public Board(int i, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getX() {return 5;}
+
+	public  boolean[] changePosition (int pos) {
+		boolean[] b= {true,false,true,true};
+		return b;
+	}
+}
