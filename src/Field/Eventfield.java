51efb0495bbc9aa67bc5f147b83b3f26adcd3158
DerModMaster <dermodmaster@googlemail.com>
2018-12-02 15:28:23 +0100

    Added Logic behind Field Package

---
+++ src/Field/Eventfield.java
@@ -0,0 +1,10 @@
+package Field;
+import Field.Field;
+
+public class Eventfield extends Field{
+    public Eventfield(int id, boolean[] wallArray){
+        super(id, wallArray);
+
+    }
+    //Event fieldevent;  //Event will be implemented soon.
+}
---
+++ src/Field/Startfield.java
@@ -0,0 +1,9 @@
+package Field;
+        import Field.Field;
+
+public class Startfield extends Field{
+    public Startfield(int id, boolean[] wallArray){
+        super(id, wallArray);
+
+    }
+}
