47d391fcb89efa5d3456681a76ba2b2bf736770e
Wambo04 <45482648+Wambo04@users.noreply.github.com>
2018-12-02 18:39:06 +0100

    Update InputController.java

--- src/TUI/InputController.java
+++ src/TUI/InputController.java
@@ -5,8 +5,8 @@ import Player.*;
 import Board.*;
 
 public class InputController{
-	Board board=new Board(5,7);
-	Player p = new Player("Player",1,1,board);
+	private Board board=new Board(5,7);
+	private Player p = new Player("Player",1,1,board);
 	public void eingabe(){
 	String eingabe=null;
 	System.out.println("Where do you want to go?(North,South,West,East):");
