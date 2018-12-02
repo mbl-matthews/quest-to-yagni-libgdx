ae9ed98fa733fa1ec66c1e5d8ab4a970c2f7665a
Wambo04 <45482648+Wambo04@users.noreply.github.com>
2018-12-02 18:15:48 +0100

    Update Finishfield.java

--- src/Field/Finishfield.java
+++ src/Field/Finishfield.java
@@ -1,15 +1,1 @@
-package Field;
-import Field.Field;
 
-public class Finishfield extends Field{
-
-    /**
-     * @param id ID of Field Object, usually set by Board-Class
-     * @param wallArray Bool-Array for Walls. [0] -> Above Wall, [1] -> Bottom Wall, [2] -> Left Wall, [3] -> Right Wall
-     */
-    public Finishfield(int id, boolean[] wallArray){
-        super(id, wallArray);
-
-    }
-    //Event fieldevent;  //Event will be implemented soon.
-}
