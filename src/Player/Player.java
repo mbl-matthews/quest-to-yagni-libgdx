a79355a89e59e342eb3b4ae826a2eee73beef2c9
Batschko <lukas.h2@hotmail.de>
2018-12-02 16:36:09 +0100

    Player update

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -6,6 +6,7 @@ class Player {
 	String name;
 	int xPos,yPos;
 	final int breite=Board.getX();
+	boolean[] walls=null;
 	
 	/**
 	 * sets starting position on the board
@@ -15,35 +16,37 @@ class Player {
 	public Player(int xPos,int yPos) {
 		this.xPos=xPos;
 		this.yPos=yPos;
+		walls=Board.changePosition(breite*xPos+yPos);
 	}
 	
 	/**
-	 * moves x,y in given direction and returns the walls
+	 * moves x,y in given direction and saves walls at position
 	 * @param direction
-	 * @return walls as boolean Array
+	 * @return true if move is possible
 	 */
-	public boolean[] move(String direction) {
-		boolean[] walls=null;
-		if(direction.toLowerCase().equals("north")) {
+	public boolean move(String direction) {
+		
+		if(direction.toLowerCase().equals("north")&& walls[0]==true) {
 			yPos+=1;
 			walls=Board.changePosition(breite*xPos+yPos);
-			return walls;
+			return true;
 		}
-		else if(direction.toLowerCase().equals("south")) {
+		else if(direction.toLowerCase().equals("south")&& walls[1]==true) {
 			yPos-=1;
 			walls=Board.changePosition(breite*xPos+yPos);
-			return walls;
+			return true;
 		}	
-		else if(direction.toLowerCase().equals("west")) {
+		else if(direction.toLowerCase().equals("west")&& walls[2]==true) {
 			xPos+=1;
 			walls=Board.changePosition(breite*xPos+yPos);
-			return walls;
+			return true;
 		}	
-		else {
+		else if(direction.toLowerCase().equals("east")&& walls[3]==true){
 			xPos+=1;
 			walls=Board.changePosition(breite*xPos+yPos);
-			return walls;
+			return true;
 		}
+		return false;
 	}
 
 }
