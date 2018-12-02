28ab58b20f8229d6a4fc5f39d3cf1be6b0c88ef9
DerModMaster <dermodmaster@googlemail.com>
2018-12-02 17:55:19 +0100

    Changed visibilities of constructors in field classes

--- src/Field/Finishfield.java
+++ src/Field/Finishfield.java
@@ -7,7 +7,7 @@ public class Finishfield extends Field{
      * @param id ID of Field Object, usually set by Board-Class
      * @param wallArray Bool-Array for Walls. [0] -> Above Wall, [1] -> Bottom Wall, [2] -> Left Wall, [3] -> Right Wall
      */
-    Finishfield(int id, boolean[] wallArray){
+    public Finishfield(int id, boolean[] wallArray){
         super(id, wallArray, "Finishfield");
 
     }
--- src/Field/Playfield.java
+++ src/Field/Playfield.java
@@ -7,7 +7,7 @@ public class Playfield extends Field{
      * @param id ID of Field Object, usually set by Board-Class
      * @param wallArray Bool-Array for Walls. [0] -> Above Wall, [1] -> Bottom Wall, [2] -> Left Wall, [3] -> Right Wall
      */
-    Playfield(int id, boolean[] wallArray){
+    public Playfield(int id, boolean[] wallArray){
         super(id, wallArray, "Playfield");
 
     }
