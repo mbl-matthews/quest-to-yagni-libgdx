e9b9375e07b9ff8e74e7789f5c87a9e3864f5b78
Batschko <lukas.h2@hotmail.de>
2018-12-02 18:41:19 +0100

    Player update

--- src/Player/JunitTestPlayer.java
+++ src/Player/JunitTestPlayer.java
@@ -7,12 +7,22 @@ import Board.Board;
 
 public class JunitTestPlayer {
 
-	Board board = new Board();
+	Board board = new Board(5,7);
 	@Test
-	void testMove() {
+	void testRichtung() {
 		Player p = new Player("Player1",1,1,board );
 		Assertions.assertEquals(true, p.move("north"),"Die Richtung ist blockiert");
-		Assertions.assertEquals(false, p.move("west"),"Die Richtung ist blockiert");
+		Assertions.assertEquals(false, p.move("south"),"Die Richtung ist blockiert");
+		Assertions.assertEquals(true, p.move("west"),"Die Richtung ist blockiert");
+		Assertions.assertEquals(true, p.move("east"),"Die Richtung ist blockiert");
 	}
+	
+	
+	@Test
+	void testName() {
+		Player p = new Player("Player1",1,1,board );
+		Assertions.assertEquals("Player1", p.getName());
+	}
+
 
 }
