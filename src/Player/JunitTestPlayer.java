ecbf372f27dca679b2a8ee1abf04f661689fc813
Batschko <lukas.h2@hotmail.de>
2018-12-02 17:40:25 +0100

    Player update

--- src/Player/JunitTestPlayer.java
+++ src/Player/JunitTestPlayer.java
@@ -3,12 +3,14 @@ package Player;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import Board.Board;
+
 public class JunitTestPlayer {
 
-	
+	Board board = new Board();
 	@Test
 	void testMove() {
-		Player p = new Player(1,1);
+		Player p = new Player("Player1",1,1,board );
 		Assertions.assertEquals(true, p.move("north"),"Die Richtung ist blockiert");
 		Assertions.assertEquals(false, p.move("west"),"Die Richtung ist blockiert");
 	}
--- src/Player/Player.java
+++ src/Player/Player.java
@@ -5,7 +5,8 @@ class Player {
 	
 	String name;
 	int xPos,yPos;
-	final int breite=Board.getX();
+	Board board = null;
+	final int width=board.getX();
 	boolean[] walls=null;
 	
 	/**
@@ -13,12 +14,15 @@ class Player {
 	 * @param xPos
 	 * @param yPos
 	 */
-	public Player(int xPos,int yPos) {
+	public Player(String name,int xPos,int yPos,Board board) {
 		this.xPos=xPos;
 		this.yPos=yPos;
-		walls=Board.changePosition(breite*yPos+xPos);
+		walls=board.changePosition(width*yPos+xPos);
+		this.board= board;
+		
 	}
 	
+
 	/**
 	 * moves x,y in given direction and saves walls at position
 	 * @param direction
@@ -28,22 +32,22 @@ class Player {
 		
 		if(direction.toLowerCase().equals("north")&& walls[0]==true) {
 			yPos+=1;
-			walls=Board.changePosition(breite*yPos+xPos);
+			walls=board.changePosition(width*yPos+xPos);
 			return true;
 		}
 		else if(direction.toLowerCase().equals("south")&& walls[1]==true) {
 			yPos-=1;
-			walls=Board.changePosition(breite*yPos+xPos);
+			walls=board.changePosition(width*yPos+xPos);
 			return true;
 		}	
 		else if(direction.toLowerCase().equals("west")&& walls[2]==true) {
 			xPos-=1;
-			walls=Board.changePosition(breite*yPos+xPos);
+			walls=board.changePosition(width*yPos+xPos);
 			return true;
 		}	
 		else if(direction.toLowerCase().equals("east")&& walls[3]==true){
 			xPos+=1;
-			walls=Board.changePosition(breite*yPos+xPos);
+			walls=board.changePosition(width*yPos+xPos);
 			return true;
 		}
 		return false;
