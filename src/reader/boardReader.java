1328e68c0db28628b066f19197e82fd25c121eed
Mathis <mbl.85@web.de>
2018-12-02 17:38:17 +0100

    Fixed boardReader

--- src/reader/boardReader.java
+++ src/reader/boardReader.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 
 import Board.Board;
 import Field.Field;
+import Field.Finishfield;
+import Field.Playfield;
+import Field.Startfield;
 
 public class boardReader {
 	
@@ -15,11 +18,11 @@ public class boardReader {
 		for(int i = 0;i<allFields.length;i++) {
 			String[] token = allFields[i].split(",");
 			if(token[4].equals("start")) {
-				fields.add(new Startfield(new boolean[] {Boolean.parseBoolean(token[0]), Boolean.parseBoolean(token[1]),Boolean.parseBoolean(token[2]),Boolean.parseBoolean(token[3])}));
+				fields.add(new Startfield(i,new boolean[] {Boolean.parseBoolean(token[0]), Boolean.parseBoolean(token[1]),Boolean.parseBoolean(token[2]),Boolean.parseBoolean(token[3])}));
 			} else if (token[4].equals("finish")) {
-				fields.add(new Finishfield(new boolean[] {Boolean.parseBoolean(token[0]), Boolean.parseBoolean(token[1]),Boolean.parseBoolean(token[2]),Boolean.parseBoolean(token[3])}));
+				fields.add(new Finishfield(i,new boolean[] {Boolean.parseBoolean(token[0]), Boolean.parseBoolean(token[1]),Boolean.parseBoolean(token[2]),Boolean.parseBoolean(token[3])}));
 			} else {
-				fields.add(new Playfield(new boolean[] {Boolean.parseBoolean(token[0]), Boolean.parseBoolean(token[1]),Boolean.parseBoolean(token[2]),Boolean.parseBoolean(token[3])}));
+				fields.add(new Playfield(i,new boolean[] {Boolean.parseBoolean(token[0]), Boolean.parseBoolean(token[1]),Boolean.parseBoolean(token[2]),Boolean.parseBoolean(token[3])}));
 			}
 		}
 		
