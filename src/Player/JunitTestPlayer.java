6c39a766ecf2d97f371f94581848648e975c0557
Wambo04 <45482648+Wambo04@users.noreply.github.com>
2018-12-02 18:17:20 +0100

    Update JunitTestPlayer.java

--- src/Player/JunitTestPlayer.java
+++ src/Player/JunitTestPlayer.java
@@ -1,16 +1,1 @@
-package Player;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-public class JunitTestPlayer {
-
-	
-	@Test
-	void testMove() {
-		Player p = new Player(1,1);
-		Assertions.assertEquals(true, p.move("north"),"Die Richtung ist blockiert");
-		Assertions.assertEquals(false, p.move("west"),"Die Richtung ist blockiert");
-	}
-
-}
