4be82498ae00c9648d435eaff2f68e1a3cca0aee
Wambo04 <45482648+Wambo04@users.noreply.github.com>
2018-12-02 18:16:11 +0100

    Update Playfield.java

--- src/Field/Playfield.java
+++ src/Field/Playfield.java
@@ -1,14 +1,1 @@
-package Field;
-import Field.Field;
 
-public class Playfield extends Field{
-
-    /**
-     * @param id ID of Field Object, usually set by Board-Class
-     * @param wallArray Bool-Array for Walls. [0] -> Above Wall, [1] -> Bottom Wall, [2] -> Left Wall, [3] -> Right Wall
-     */
-    public Playfield(int id, boolean[] wallArray){
-        super(id, wallArray);
-
-    }
-}
