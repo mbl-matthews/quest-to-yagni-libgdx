39de2d23dc02984cafa6654ebf4958a8b770f919
DerModMaster <dermodmaster@googlemail.com>
2018-12-02 19:07:02 +0100

    Created Java-Docs

--- src/Event/Hintevent.java
+++ src/Event/Hintevent.java
@@ -2,16 +2,26 @@ package Event;
 
 public class Hintevent extends Event{
 
+    /**
+     * @param id    ID of Event
+     * @param name  Name of Event
+     */
     public Hintevent(int id,String name){
         super(id,name);
     }
 
+    /**
+     * Triggers the Event Method
+     */
     @Override
     public void triggerEvent() {
 
     }
 
-    public void hintEvent(){
+    /**
+     * Creating a hint Event. It will print a hint.
+     */
+    void hintEvent(){
 
     }
 }
